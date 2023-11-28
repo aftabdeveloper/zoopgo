00132a486a6d7fe457465d9ddbd4a2849df458fd
Aftab Alam <alam69745@gmail.com>
2023-11-28 23:50:56 +0530

    brand logo added

--- component/shared/layout/index.jsx
+++ component/shared/layout/index.jsx
@@ -1,12 +1,14 @@
 import Link from "next/link"
+import Image from "next/image"
 import {
-    InboxOutlined
+    InboxOutlined,
+    MobileOutlined
 } from '@ant-design/icons'
 
 const Layout = ()=>{
     return (
         <>
-           <nav className="bg-slate-900 text-white flex justify-between p-3 px-9">
+           <nav className="bg-slate-900 text-white flex justify-between p-3 px-9 items-center">
              <h1 className="capitalize">india's most trusted platform</h1>
              <div className="flex gap-8">
                     <div className="flex items-center gap-1">
@@ -25,9 +27,18 @@ const Layout = ()=>{
                 </Link>
              </div>
            </nav>
-           <nav className="bg-white shadow-xl p-6">
-            <h1>Brand</h1>
-            <p>gg</p>
+           <nav className="bg-white shadow-xl p-3 px-9 flex justify-between items-center">
+            <Link href="#">
+                <Image 
+                    src="/images/brand.png"
+                    width={120}
+                    height={60}
+                />
+            </Link>
+            <div className="flex items-center gap-2">
+                <MobileOutlined />
+                <p>9971746885</p>    
+            </div>
            </nav>
         </>
     )
